a57ef66b0a9528a60b7f05e65c1543821241e400
Gleison2015SP <gleison.freitas@hotmail.com>
2025-05-18 19:13:24 -0300

    feat: use REACT_APP_API_URL environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production'
-    ? 'https://musicrepertory-backend.onrender.com/api'
-    : 'http://localhost:3001/api'
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
